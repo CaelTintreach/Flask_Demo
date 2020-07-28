b6de841ed465c95bb99b5e79ebe4f1822694839e
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-07-28 10:25:37 +0100

    Caught up and added login functionality.

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+from os import getenv
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+bcrypt = Bcrypt(app)
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 db = SQLAlchemy(app)
 
-from application import routes
-
-from os import getenv
-
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+
+from application import routes
